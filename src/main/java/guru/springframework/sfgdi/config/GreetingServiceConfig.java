de5e7228f55821a73e37f4aceb84bfb7066c00a6
bourisdim <bourisdim@outlook.com>
2022-09-24 18:02:19 +0300

    Change Greeting Services to use java based configuration instead of component scan using @Configuration and @Bean annotations

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,12 +1,8 @@
 package guru.springframework.sfgdi.config;
 
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
+import guru.springframework.sfgdi.services.*;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -24,5 +20,23 @@ public class GreetingServiceConfig {
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService(){
         return new PropertyInjectedGreetingService();
+    }
+
+   @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES","default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
     }
 }
