baf60c214e74b3a7a4f78e07629f9c8ff4ca81c3
bourisdim <bourisdim@outlook.com>
2022-09-25 11:43:41 +0300

    Creating a dummy datasource to demonstrate applicaton properties - env variabls - program args
    Properties profiles -qa, -dev

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,14 +1,27 @@
 package guru.springframework.sfgdi.config;
 
 
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repostitories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repostitories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+
 
 @Configuration
 public class GreetingServiceConfig {
 
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}")String username,
+                                  @Value("${guru.password}")String password,
+                                  @Value("${guru.jdbcurl}") String jdbcurl){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+        return fakeDataSource;
+    }
     @Bean
     ConstructorGreetingService constructorGreetingService(){
         return new ConstructorGreetingService();
